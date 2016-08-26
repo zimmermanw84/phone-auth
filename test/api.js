bd9d75d335d129321a567f297ad516dd3e88bf64
Walter Zimmerman <zimmermanw84@gmail.com>
2016-08-25 18:14:38 -0700

    setup more api test

--- test/api.js
+++ test/api.js
@@ -18,6 +18,8 @@ const api = supertest(app);
 
 let TEST_USER;
 
+let verification_code;
+
 describe(`Test to ensure the API is returning correct status codes and data`, () => {
 	// Setup
 	before(() => {
@@ -28,7 +30,29 @@ describe(`Test to ensure the API is returning correct status codes and data`, ()
 	it(`GET /users/:phone_number should return 401 with no authorization token`, (done) => {
 		api.get('/users/1231234567')
 			.expect(401, done);
-	})
+	});
+
+	it(`GET /users/:phone_number should return 200 with authorization`, (done) => {
+		// Get users auth and setup for verification_code test
+		TEST_USER.findBy('phone_number', "1231234567")
+			.then((u) => {
+				// Setup for next test
+				verification_code = u.verification_code;
+				api.get('/users/1231234567')
+					.set("authorization", u.auth_token)
+					.expect(200, done);
+			})
+			.catch((err) => {
+				// should not reach this block
+				expect(err).to.be.undefined;
+				done();
+			});
+	});
+
+	// it(`POST /users/:phone_number/verify should return 200`)
+
+	// it(`POST /users/:phone_number/verify should return authorization token`)
+	// it(`POST /users/:phone_number/verify should return 401 with invalid verification_code`)
 
 	after(() => {
 		TEST_USER.destroy();
